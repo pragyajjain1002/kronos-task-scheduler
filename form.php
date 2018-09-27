f531f03d93026e7915c0ec71268c86a7cdb0be79
Abhay Singh <abhisinghrathore1998@gmail.com>
2018-09-27 16:13:16 +0530

    latest form.php

--- form.php
+++ form.php
@@ -1,23 +1,36 @@
+  <link rel="stylesheet" type="text/css" href="main.css">
+<div>
 <form method="post" action="test.php">
 <input name="last_name" placeholder="Last name">
 <input name="dept" placeholder="Department">
 <input name="id" placeholder="Id">
-<input type="submit" name="q1">
+<input type="submit" name="q1" value="Search">
 </form>
-<br>
-<form method="post" action="test.php">
 
+</div>
+<div>
+<form method="post" action="test.php">
 <input type="submit" value="Largest department" name="q2">
 </form>
+</div>
+<div>
+
 <form method="post" action="test.php">
 <input name="dept_no" placeholder="Department Number">
 <input type="submit" value="Order by tenure" name="q3">
 </form>
+</div>
+<div>
+
 <form method="post" action="test.php">
 <input name="dept_no" placeholder="Department Number">
 <input type="submit" value="Gender Ratio" name="q4">
 </form>
+</div>
+<div>
 <form method="post" action="test.php">
 <input name="dept_no" placeholder="Department Number">
+<input name="title" placeholder="Title">
 <input type="submit" value="Income Ratio" name="q5">
 </form>
+</div>
