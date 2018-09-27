12aee82369c4266a7cab18c764856eb2f8804376
Abhay Singh <abhisinghrathore1998@gmail.com>
2018-09-27 15:15:39 +0530

    updated form.php

--- form.php
+++ form.php
@@ -1,6 +1,23 @@
 <form method="post" action="test.php">
-<input name="last_name">
-<input name="dept">
-<input name="id">
-<input type="submit">
+<input name="last_name" placeholder="Last name">
+<input name="dept" placeholder="Department">
+<input name="id" placeholder="Id">
+<input type="submit" name="q1">
+</form>
+<br>
+<form method="post" action="test.php">
+
+<input type="submit" value="Largest department" name="q2">
+</form>
+<form method="post" action="test.php">
+<input name="dept_no" placeholder="Department Number">
+<input type="submit" value="Order by tenure" name="q3">
+</form>
+<form method="post" action="test.php">
+<input name="dept_no" placeholder="Department Number">
+<input type="submit" value="Gender Ratio" name="q4">
+</form>
+<form method="post" action="test.php">
+<input name="dept_no" placeholder="Department Number">
+<input type="submit" value="Income Ratio" name="q5">
 </form>
